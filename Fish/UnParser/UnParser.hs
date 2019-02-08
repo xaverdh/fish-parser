ebc6f875ace091acf0d20f1d1b90c0ddbcbfeefb
Dominik Xaver Hörl <hoe.dom@gmx.de>
2019-02-08 19:48:09 +0100

    The ? wildcard has been removed -- leftovers

--- Fish/UnParser/UnParser.hs
+++ Fish/UnParser/UnParser.hs
@@ -242,7 +242,6 @@ instance Unparse Glob where
   unparse = \case
     StarGl -> "*"
     DiStarGl -> "**"
-    QMarkGl -> "?"
 
 instance Unparse (VarIdent T.Text t) where
   unparse (VarIdent _ s) = extractText s
